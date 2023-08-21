b5b65638f69010ebe56134996513b45c19d04231
Curve <fynnbwdt@gmail.com>
2023-08-22 01:34:16 +0200

    feat: update lua-hook

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,99 @@
-#include <exception>
-#include <windows.h>
-#include <lime/module.hpp>
-#include <lime/hooks/detour.hpp>
-
-#include "lua.hpp"
 #include "logger.hpp"
-#include "constants.hpp"
+#include "manager.hpp"
 
-using chalchiu::logger;
+#include <lua.hpp>
+#include <lime/module.hpp>
+#include <lime/entrypoint.hpp>
+#include <lime/hooks/hook.hpp>
 
-lua_State *lua_state;
-std::unique_ptr<lime::detour> detour_corona_init;
-std::unique_ptr<lime::detour> detour_module_loader;
+using loader::logger;
 
-void hk_module_loader(lua_State *state, void *param_2, int param_3, unsigned int param_4)
+void lime::load()
 {
-    logger::get()->debug("[module_loader] 0x{}", reinterpret_cast<std::uintptr_t>(state));
-
-    if (!lua_state)
-    {
-        lua_state = state;
-    }
-
-    return detour_module_loader->get_original<decltype(hk_module_loader) *>()(state, param_2, param_3, param_4);
-}
-
-bool hk_corona_init(int param, HWND *hwnd)
-{
-    auto original = detour_corona_init->get_original<decltype(hk_corona_init) *>()(param, hwnd);
-
-    detour_corona_init.reset();
-    detour_module_loader.reset();
-
-    chalchiu::lua::init(lua_state);
-    return original;
-}
-
-extern "C" BOOL APIENTRY DllMain(HANDLE, DWORD reason, LPVOID)
-{
-    if (reason != DLL_PROCESS_ATTACH)
-    {
-        return TRUE;
-    }
-
-    CHAR file[MAX_PATH];
-    GetModuleFileNameA(nullptr, file, MAX_PATH);
+    logger::get()->info("all the nuts roll down to florida!");
 
     auto corona = lime::module::get("CoronaLabs.Corona.Native.dll");
 
     if (!corona)
     {
-        logger::get()->warn("No CoronaSDK [{}] found ({})", corona.has_value(), file);
-        return TRUE;
+        logger::get()->error("couldn't find \"CoronaLabs.Corona.Native.dll\"");
+        return;
     }
 
-    logger::get()->info("Loading Chalchiu (v{}) [{}]", chalchiu::version, file);
-    logger::get()->info("Found CoronaSDK at 0x{}", corona->get_start(), file);
+    auto version_string = corona->symbol("CoronaVersionBuildString");
+    using version_string_t = char (*)();
 
-    auto module_loader = corona->get_symbol("CoronaLuaRegisterModuleLoader");
-    auto corona_init = corona->get_symbol("CoronaWin32RuntimeRun");
-
-    if (!module_loader || !corona_init)
+    if (!version_string)
     {
-        logger::get()->error("No 'CoronaWin32RuntimeRun' [{}] or 'CoronaLuaRegisterModuleLoader' [{}] found",
-                             corona_init, module_loader);
-
-        return TRUE;
+        logger::get()->error("couldn't find \"CoronaVersionBuildString\"");
+        return;
     }
 
-    logger::get()->info("Found 'CoronaLuaRegisterModuleLoader' at 0x{}", module_loader);
+    const auto version = reinterpret_cast<version_string_t>(version_string)();
+    logger::get()->info("running on corona version \"{}\"", version);
 
-    std::set_terminate([] {
-        logger::get()->error("[crash] Unhandled exception!");
+    auto lua = lime::module::get("lua.dll");
 
-        try
-        {
-            std::rethrow_exception(std::current_exception());
-        }
-        catch (const std::exception &e)
-        {
-            logger::get()->error("[crash] [{}] {}", typeid(e).name(), e.what());
-        }
-        catch (...)
-        {
-            logger::get()->error("[crash] Unknown exception '{}'", typeid(std::current_exception()).name());
-        }
+    if (!lua)
+    {
+        logger::get()->error("couldn't find \"lua.dll\"");
+        return;
+    }
 
-        std::abort();
-    });
+    auto push_string = lua->symbol("lua_pushstring");
+    using push_string_t = const char *(lua_State *, const char *);
 
-    detour_module_loader = lime::detour::create(module_loader, hk_module_loader);
-    detour_corona_init = lime::detour::create(corona_init, hk_corona_init);
+    if (!push_string)
+    {
+        logger::get()->error("couldn't find \"lua_pushstring\"");
+        return;
+    }
 
-    return TRUE;
+    lime::hook<push_string_t>::create(push_string,
+                                      [](auto *hook, lua_State *state, const char *name)
+                                      {
+                                          auto rtn = hook->original()(state, name);
+
+                                          if (strcmp(name, "coronabaselib") == 0)
+                                          {
+                                              auto state_addr = reinterpret_cast<std::uintptr_t>(state);
+                                              logger::get()->debug("lua state is at {0:#x}", state_addr, name);
+
+                                              loader::manager::init(state);
+                                              delete hook;
+                                          }
+
+                                          return rtn;
+                                      });
+
+    auto get_field = lua->symbol("lua_getfield");
+    using get_field_t = int(lua_State *, int, const char *);
+
+    if (!get_field)
+    {
+        logger::get()->error("couldn't find \"lua_getfield\"");
+        return;
+    }
+
+    lime::hook<get_field_t>::create(get_field,
+                                    [](auto *hook, lua_State *state, int index, const char *name)
+                                    {
+                                        auto rtn = hook->original()(state, index, name);
+
+                                        if (strcmp(name, "shaderPrecision") == 0)
+                                        {
+                                            auto state_addr = reinterpret_cast<std::uintptr_t>(state);
+                                            logger::get()->debug("loading shader precision in {0:#x}", state_addr);
+
+                                            loader::manager::ready();
+                                            delete hook;
+                                        }
+
+                                        return rtn;
+                                    });
+}
+
+void lime::unload()
+{
+    logger::get()->info("unload");
 }
